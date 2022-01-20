40d56a53ab76921423cacfb25ad16208cb70843a
yarah <yarah@LAPTOP-VAK29FON>
2022-01-19 20:04:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
